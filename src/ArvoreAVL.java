70c380950200feddc376fe095e913271ac744a94
onooryu <matheusryu53@gmail.com>
2025-06-03 20:22:14 -0300

    Implementando ArvoreAVL

---
+++ src/ArvoreAVL.java
@@ -0,0 +1,20 @@
+public class ArvoreAVL {
+    No raiz;
+
+    int altura (NoAvl No){
+        if (No == null){
+            return 0;
+        } return No.altura;
+    }
+
+    int getBalanceamento (NoAvl No){
+        if (No == null){
+            return 0;
+        } return altura(No.esquerda) - altura(No.direita);
+    }
+
+    void atualizarAltura (NoAvl No){
+        No.altura = 1 + Math.max(altura(No.esquerda), altura(No.direita));
+    }
+
+}
